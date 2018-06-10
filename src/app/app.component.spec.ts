18ffca8349ca304071dda91a4c3c514e813f35c4
Samarth <samarth@newidea.com>
2018-06-11 03:34:38 +0530

    Fix Component Name Error in Spec Test
    Name of AppComponent was changed to AppRootComponent. The spec test
    source code is modified to reflect the name chnage of component.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,30 +1,30 @@
 import { TestBed, async } from "@angular/core/testing";
-import { AppComponent } from "./app.component";
+import { AppRootComponent } from "./app.component";
 
 
-describe("AppComponent", () => {
+describe("AppRootComponent", () => {
     beforeEach(async(() => {
         TestBed.configureTestingModule({
             declarations: [
-                AppComponent
+                AppRootComponent
             ],
         }).compileComponents();
     }));
 
     it("should create the app", async(() => {
-        const fixture = TestBed.createComponent(AppComponent);
+        const fixture = TestBed.createComponent(AppRootComponent);
         const app = fixture.debugElement.componentInstance;
         expect(app).toBeTruthy();
     }));
 
     it(`should have as title "app"`, async(() => {
-        const fixture = TestBed.createComponent(AppComponent);
+        const fixture = TestBed.createComponent(AppRootComponent);
         const app = fixture.debugElement.componentInstance;
         expect(app.title).toEqual("app");
     }));
 
     it("should render title in a h1 tag", async(() => {
-        const fixture = TestBed.createComponent(AppComponent);
+        const fixture = TestBed.createComponent(AppRootComponent);
         fixture.detectChanges();
         const compiled = fixture.debugElement.nativeElement;
         expect(compiled.querySelector("h1").textContent).toContain("Welcome to awesome-idea!");
